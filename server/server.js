d2922cc6a696a789a62a14795be4d9da3cc5f2fe
Sushil Chhetri <chhetrisushil@gmail.com>
2014-06-17 16:38:53 +0530

    adding elegant way for page serving

--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
 var application_root 		 = __dirname,
 	express              	 = require('express'),
 	server               	 = express(),
-	db					 	 = require('./database/database.js'),
+	db			 = require('./database/database.js'),
 	bodyParser           	 = require('body-parser'),
 	methodOverride       	 = require('method-override'),
-	errorHandler		 	 = require('express-error-handler'),
-	routes				 	 = require('./router/routes.js'),
-	restApi				 	 = require('./api/restApi.js');
+	errorHandler		 = require('express-error-handler'),
+	routes			 = require('./router/routes.js'),
+	restApi			 = require('./api/restApi.js'),
+	ejs			 = require('ejs'),
+	path			 = require('path');
 
 server.use(bodyParser());
 server.use(methodOverride());
 server.use(errorHandler({ dumpExceptions: true, showStack: true }));
 server.use(express.static('client/static/'));
+server.set('views', path.join(application_root, '..', '/client'));
+server.engine('html', ejs.renderFile);
 		
 //setup page routes
 routes(server);
